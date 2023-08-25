1357bc10498b83e6d84ce0a018f4d46954a08428
Fariv <a.f.fariv@gmail.com>
2023-08-26 03:20:05 +0600

    product details styling on progress

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -6,14 +6,33 @@ const ProductDetailsStyled = styled.div`
     display: flex;
     flex-direction: column;
     width: 50%;
-    margin-top: 4rem;
+    padding-top: 4.5rem;
+    padding-left: 4.5rem;
 `;
+
+const SubtitleStyled = styled.div`
+    color: hsl(26, 100%, 55%);
+    font-weight: 700;
+    font-size: 15px;
+    letter-spacing: 1px;
+`;
+
+const HeaderTextStyled = styled.h1`
+    font-size: 46px;
+    margin: 20px 0;
+`;
+
+const ParagraphStyled = styled.p`
+    color: hsl(219, 9%, 45%);
+    line-height: 1.6;
+`;
+
 const ProductDetails = () => {
     return (
         <ProductDetailsStyled>
-            <div>SNEAKER COMPANY</div>
-            <h1>Fall Limited Edition Sneakers</h1>
-            <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+            <SubtitleStyled>SNEAKER COMPANY</SubtitleStyled>
+            <HeaderTextStyled>Fall Limited Edition Sneakers</HeaderTextStyled>
+            <ParagraphStyled>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</ParagraphStyled>
             <div>
                 <span>$125.00</span>
                 <span className='badge'>50%</span>
